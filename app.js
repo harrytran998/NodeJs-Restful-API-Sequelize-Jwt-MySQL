5e5afa3e3685160b195bc569b16c059f458df2bc
Nhat <jetaimefrc@gmail.com>
2018-09-27 11:18:32 +0700

    [Update] Validate User

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
     min: 0,
     acquire: 30000,
     idle: 10000
-  }
+  },
+  port: 6996
 })
+
 sequelize.authenticate().then(() => {
   console.log('connect succes !');
 }).catch(err => console.log(`Can't connect, ${err} !`))
@@ -27,22 +29,51 @@ const User = sequelize.define('user', {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      len: [2, 25]
+      len: {
+        args: [2, 25],
+        msg: `Password length must be in range [${args[0]}, ${args[1]}]`
+      },
+      /**
+       * @param {String} bodyVal
+       */
+      startWihtUpperCase: (bodyVal) => {
+        let first = bodyVal.charAt(0)
+        let check = first.toUpperCase === first
+        if(!check){
+          throw new Error('First character of password must be a uppercase character')
+        }
+      },
+      /**
+       * @param {String} val
+       */
+      containsNumber: (val) => {
+        let check =  /\d/.test(val)
+        if(!check){
+          throw new Error('Password must contains at lest 1 number')
+        }
+      } 
     }
   }
+}, {
+  timestamps: false,
+  tableName: 'User',
+  freezeTableName: true,
+  version: true
 });
 
-// sequelize.sync().then(() => {
-//   User.create({
-//     username: 'jetaimefrc',
-//     password: '123456'
-//   })
-// }).catch((err) => console.log(err))
+sequelize.sync({
+  force: true,
+  logging: console.log()
+}).then(() => {
+  User.create({
+    username: 'jetaimefrc1',
+    password: 'jetaime123'
+  })
+}).catch((err) => console.log(err))
 
-User
-  .findById(1)
-  .then((result) => {
-  console.log(result.dataValues);  
-}).catch(() => console.log('Not found !'))
+// User
+//   .findById(1)
+//   .then((result) => {
+//   console.log(result.dataValues);  
+// }).catch(() => console.log('Not found !'))
 
-module.exports = User
