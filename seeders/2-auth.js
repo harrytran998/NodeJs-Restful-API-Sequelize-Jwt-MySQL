96ccc08f66c0290b8b88625467df1bb4b3b108ba
Nhat <jetaimefrc@gmail.com>
2018-10-03 18:09:47 +0700

    [Create] data User & Authentication

--- seeders/2-auth.js
+++ seeders/2-auth.js
@@ -5,16 +5,42 @@ module.exports = {
   up: async (queryInterface) => {
     const salt = await bcrypt.genSalt(10);
     return queryInterface.bulkInsert('Auth', [{
-      username: 'nhatdeeptrai',
-      password: await bcrypt.hash('password', salt),
+      username: 'abcd123',
+      password: await bcrypt.hash('Love123', salt),
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {});
+    },{
+      username: 'abcd456',
+      password: await bcrypt.hash('Love123', salt),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd789',
+      password: await bcrypt.hash('Love123', salt),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd1011',
+      password: await bcrypt.hash('Love123', salt),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd1213',
+      password: await bcrypt.hash('Love123', salt),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd1415',
+      password: await bcrypt.hash('Love123', salt),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ], {});
   },
 
   down: (queryInterface) => {
     queryInterface.bulkDelete('Auth', [{
-      username: 'nhatdeeptrai'
+      // username: 'abcd1415'
     }]);
   }
 };
