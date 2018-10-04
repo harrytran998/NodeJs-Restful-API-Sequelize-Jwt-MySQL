66441e88762825157e7286aa9b66720a23c3cd22
Nhat <jetaimefrc@gmail.com>
2018-10-04 20:47:58 +0700

    [Update] refactor project

---
+++ db/migrations/1-user.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      }
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ db/seeders/1-user.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      username: 'abcd123',
+      fullName: 'Tran Quang Nhat',
+      email: 'demo@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      username: 'abcd456',
+      fullName: 'Pham Ngoc Hoa',
+      email: 'demo1@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd789',
+      fullName: 'Do Quang Hiep',
+      email: 'demo2@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd1011',
+      fullName: 'Nguyen Tien Manh',
+      email: 'demo3@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd1213',
+      fullName: 'Phan Van Duc',
+      email: 'demo4@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      username: 'abcd1415',
+      fullName: 'Nguyen Xuan Cuong',
+      email: 'demo5@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Users', [{
+      // firstName: 'Tran Quang Nhat'
+    }]);
+  }
+};
